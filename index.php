125b1dfa7e20693723173c9b29254b2739535447
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-03-07 14:45:40 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * unit-apcu:/index.php
+ *
+ * @creation  2020-02-18
+ * @version   1.0
+ * @package   unit-apcu
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+if(!function_exists('apcu_add') ){
+	throw new Exception("APCu has not been installed.");
+}
+
+//	...
+include(__DIR__.'/APCU.trait.php');
+include(__DIR__.'/APCu.class.php');
