4abb355ef3c9f3746f763895f6dac7f841727bc1
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-01 20:30:28 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/** op-unit-apcu:/config.php
+ *
+ * @created		2024-01-01
+ * @version		1.0
+ * @package		op-unit-apcu
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$config = [
+	'salt' => null,
+];
+
+//	...
+return $config;
